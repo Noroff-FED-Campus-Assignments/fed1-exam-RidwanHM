59382c0f80a72dd20e2c084ff4c7ce3cfe002812
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-05-06 15:10:02 +0200

    push

--- js/contact.js
+++ js/contact.js
@@ -12,32 +12,76 @@ formEl.addEventListener("submit", (event) => {
     const subject = subjectEl.value;
     const content = contentEl.value;
 
-    if (name === "") {
-        alert("Please enter a name")
+    if (name === "" || name.length < 2) {
+        alert("Please enter a name with at least two characters")
         return;
     }
+    
 
     if (email === "") {
         alert("please enter a email")
         return;
     }
 
-    if (subject === "") {
-        alert("Should be more than 15 characters lonng")
+    if (subject === "" || subject.length < 16) {
+        alert("Subject should be more than 15 characters long")
         return;
     }
 
-    if (content === "") {
-        alert("Should be more than 25 characters long")
+    if (content === "" || content.length < 26) {
+        alert("Content should be more than 25 characters long")
+        return;
     }
 
-    alert("Form submitted")
+    alert("Form submitted!")
+});
+
+nameEl.addEventListener("blur", (event) => {
+    const email = event.target.value.trim();
+
+    const emailRegex = /^[a-zA-Z]{2,}$/;
+
+    if (emailRegex.test(email)) {
+        event.target.classList.add("is-success");
+        event.target.classList.remove("is-error");
+    } else {
+        event.target.classList.add("is-error");
+        event.target.classList.remove("is-success");
+    }
 });
 
 emailEl.addEventListener("blur", (event) => {
     const email = event.target.value.trim();
 
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+    if (emailRegex.test(email)) {
+        event.target.classList.add("is-success");
+        event.target.classList.remove("is-error");
+    } else {
+        event.target.classList.add("is-error");
+        event.target.classList.remove("is-success");
+    }
+});
+
+subjectEl.addEventListener("blur", (event) => {
+    const email = event.target.value.trim();
+
+    const emailRegex = /^.{16,}$/;
+
+    if (emailRegex.test(email)) {
+        event.target.classList.add("is-success");
+        event.target.classList.remove("is-error");
+    } else {
+        event.target.classList.add("is-error");
+        event.target.classList.remove("is-success");
+    }
+});
+
+contentEl.addEventListener("blur", (event) => {
+    const email = event.target.value.trim();
+
+    const emailRegex = /^.{26,}$/;
 
     if (emailRegex.test(email)) {
         event.target.classList.add("is-success");
