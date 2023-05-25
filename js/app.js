4931af32a618ebd0677742584595a5f299e7172b
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-05-25 23:54:10 +0200

    push

--- js/app.js
+++ js/app.js
@@ -1,30 +1,55 @@
 const resultsContainer = document.querySelector("#js-list-container");
+const loadMoreButton = document.querySelector("#load-more");
 
-const url = "http://rhm-exam-2023.local/wp-json/wp/v2/posts/"
+const postsUrl = "https://excellent-things.flywheelsites.com/wp-json/wp/v2/posts/";
+const mediaUrl = "https://excellent-things.flywheelsites.com/wp-json/wp/v2/media/";
 
-async function fetchCities () {
-    try {
-        const response = await fetch(url);
-        const json = await response.json();
-        
-        resultsContainer.innerHTML = "";
+let currentPage = 1;
+const postsPerPage = 10;
 
-        const cities = json;
+async function fetchCities() {
+  try {
+    const postsResponse = await fetch(`${postsUrl}?page=${currentPage}&per_page=${postsPerPage}`);
+    const posts = await postsResponse.json();
 
-        cities.forEach(function (city){
-            resultsContainer.innerHTML +=`
+    const mediaResponse = await fetch(mediaUrl);
+    const media = await mediaResponse.json();
 
-            <a href="/details.html?id=${city.id}">
-            <h2>${city.title.rendered}</h2>
-            <p> More Info </p>
-            </a>
-            `;
+    let html = "";
 
-        })
-    } catch (error) {
-        console.log(error);
-        resultsContainer.innerHTML =`Error: ${error}`;
+    posts.forEach(city => {
+      const featuredImageId = city.featured_media;
+      const featuredImage = media.find(image => image.id === featuredImageId);
+      const imageUrl = featuredImage ? featuredImage.source_url : "";
+
+      html += `
+        <h2>${city.title.rendered}</h2>
+        <img src="${imageUrl}" alt="Featured Image" class="city-image">
+        <a href="/details.html?id=${city.id}"><p>Press the image for more Info</p>
+      </a>`;
+    });
+
+    if (currentPage === 1) {
+      resultsContainer.innerHTML = html;
+    } else {
+      resultsContainer.innerHTML += html;
     }
+
+    if (posts.length >= postsPerPage) {
+      loadMoreButton.style.display = "block";
+    } else {
+      loadMoreButton.style.display = "none";
+    }
+  } catch (error) {
+    resultsContainer.innerHTML = `Error: ${error}`;
+  }
 }
+
+function handleLoadMoreClick() {
+  currentPage++;
+  fetchCities();
+}
+
+loadMoreButton.addEventListener("click", handleLoadMoreClick);
 
 fetchCities();
---
+++ js/carousel.js
@@ -0,0 +1,118 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const resultsContainer = document.querySelector(".swiper-wrapper");
+    const searchInput = document.getElementById("searchInput");
+    
+  
+
+    const postsUrl = "https://excellent-things.flywheelsites.com/wp-json/wp/v2/posts/";
+    const mediaUrl = "https://excellent-things.flywheelsites.com/wp-json/wp/v2/media/";
+  
+    let media = [];
+    let posts = [];
+  
+    async function fetchBlogsAndInitializeSwiper() {
+      try {
+        const postsResponse = await fetch(postsUrl);
+        posts = await postsResponse.json();
+  
+        const mediaResponse = await fetch(mediaUrl);
+        media = await mediaResponse.json();
+  
+        renderPosts(posts);
+  
+        searchInput.removeEventListener("input", handleSearchInput);
+        searchInput.addEventListener("input", handleSearchInput);
+  
+        return { media, posts };
+      } catch (error) {
+        resultsContainer.innerHTML = `Error: ${error}`;
+        return { error };
+      }
+    }
+  
+    function filterPostsByTitle(searchText) {
+      return posts.filter((post) =>
+        post.title.rendered.toLowerCase().includes(searchText.toLowerCase())
+      );
+    }
+  
+    function renderPosts(posts) {
+      let html = "";
+      posts.forEach((blog) => {
+        const featuredImageId = blog.featured_media;
+        const featuredImage = media.find((image) => image.id === featuredImageId);
+        const imageUrl = featuredImage ? featuredImage.source_url : "";
+        const title = blog.title.rendered;
+        const postId = blog.id;
+  
+        const detailsPageUrl = `/details.html?id=${postId}`;
+
+        html += `
+          <div class="swiper-slide">
+            <div class="carousel-post">
+              <div class="blogContainer">
+                <a href="${detailsPageUrl}"><img src="${imageUrl}" alt="Featured Image"></a>
+                <h1 class="city-title">${title}</h1>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+  
+      resultsContainer.innerHTML = html;
+  
+      initializeSwiper();
+    }
+  
+    async function fetchPostsAndRender() {
+      try {
+        const { error } = await fetchBlogsAndInitializeSwiper();
+        if (error) {
+          console.error("Error:", error);
+          return;
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+ 
+    function initializeSwiper() {
+      new Swiper(".swiper-container", {
+        slidesPerView: 5,
+        spaceBetween: 1,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        touch: {
+          enabled: true,
+        },
+      });
+    }
+  
+    function handleSearchInput() {
+      const searchText = searchInput.value.trim().toLowerCase();
+      const filteredPosts = filterPostsByTitle(searchText);
+      renderPosts(filteredPosts);
+    }
+  
+    fetchPostsAndRender()
+      .then(() => {
+        initializeSwiper();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+  
+ 
